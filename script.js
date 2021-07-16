30b25e143b4d8a66899978bba5d51dec04fc3004
robinwatts96 <robinwatts96@gmail.com>
2021-07-16 07:57:17 +0100

    Update

---
+++ script.js
@@ -0,0 +1,15 @@
+// const arrow = document.getElementById('testArrow');
+// const hidden = document.getElementById('testP');
+
+// function myFunction() {
+//  if (hidden.style.display === "none") {
+//     hidden.style.display = "block";
+//  } 
+// };
+
+const arrow = document.getElementById('testArrow');
+const hidden = document.getElementById('testP');
+
+arrow.onClick = function showHidden() {
+    hidden.style.display = 'block';
+}
